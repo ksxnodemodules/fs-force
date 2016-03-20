b4065c63aa6848b7a68014a045793e9e37d0cbc8
khai96_ <hvksmr1996@gmail.com>
2016-03-20 16:35:16 +0700

    update

---
+++ mkdir-sync.js
@@ -0,0 +1,38 @@
+
+((module) => {
+	'use strict';
+
+	var fs = require('fs');
+	var path = require('path');
+	var _getfunc = require('./utils/get-val.js').function;
+	var Info = require('./utils/info.js');
+	var Action = require('./utils/action.js');
+
+	var mkdirSync = fs.mkdirSync;
+	var statSync = fs.statSync;
+	var unlinkSync = fs.unlinkSync;
+	var resolvePath = path.resolve;
+	var getParent = path.dirname;
+
+	const DONOTHING = () => {};
+
+	var _mkdirSync = (dirname, onaction) => {
+		if (!dirname) {
+			return [];
+		}
+		try {
+			var info = statSync(dirname);
+			if (info.isFile()) {
+
+			}
+		} catch (_) {
+			mkdirSync(dirname);
+			var action = new Action('create', dirname, 'dir');
+			// continue from here...
+		}
+	};
+
+	module.exports = (dirname, onaction) =>
+		_mkdirSync(resolvePath(dirname), _getfunc(onaction, DONOTHING));
+
+})(module);
