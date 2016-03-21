ab4668a6d6135d09aebd50ace18149e05eaccdd7
khai96_ <hvksmr1996@gmail.com>
2016-03-21 22:20:42 +0700

    filename correction

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ module.exports = {
 	'mkdir': require('./mkdir.js'),
 	'mkdirSync': require('./mkdir-sync.js'),
 	'writeFile': require('./write-file.js'),
-	'writeFileSync': require('./writeFileSync.js'),
+	'writeFileSync': require('./write-file-sync.js'),
 	'delete': require('./delete.js'),
 	'deleteSync': require('./delete-sync.js'),
 	'utils': require('./utils')
