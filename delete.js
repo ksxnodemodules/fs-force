f002293528878f3e07105db41dc55bf0295643d9
khai96_ <hvksmr1996@gmail.com>
2016-03-21 14:53:03 +0700

    complete delete

--- delete.js
+++ delete.js
@@ -12,6 +12,7 @@
 	var rmdir = fs.rmdir;
 	var unlink = fs.unlink;
 	var stat = fs.stat;
+	var readdir = fs.readdir;
 	var resolvePath = path.resolve;
 	var getParent = path.dirname;
 
@@ -21,7 +22,49 @@
 
 	const DONOTHING = () => {};
 
-	var _rm = (entry, onfinish, onaction) => {};
+	var _rm = (entry, onfinish, onaction) => {
+		var callOnFinish = (...action) =>
+			onfinish(null, new Info('delete', entry, action));
+		stat(entry, (error, info) => {
+			if (error) {
+				return callOnFinish();
+			}
+			if (info.isFile()) {
+				return unlink(entry, (error) => {
+					if (error) {
+						return onfinish(error, null);
+					}
+					var action = new Action('delete', entry, 'file');
+					justTry(onaction, [action]);
+					callOnFinish(action);
+				});
+			}
+			if (info.isDirectory()) {
+				return readdir(entry, (error, list) => {
+					if (error) {
+						return onfinish(error, null);
+					}
+					var childpromises = new Set();
+					for (let item of list) {
+						let callback = (resolve, reject) =>
+							_rm(item, (error, info) => error ? reject(error) : resolve(info), onaction);
+						childpromises.add(new Promise(callback));
+					}
+					Promise.all(childpromises).then((info) => {
+						rmdir(entry, (error) => {
+							if (error) {
+								onfinish(error, null);
+							}
+							var action = new Action('delete', entry, 'dir');
+							justTry(onaction, [action]);
+							callOnFinish(...info.map((info) => info.action), action);
+						});
+					}).catch((error) => onfinish(error, null));
+				});
+			}
+			onfinish(new Error(`Can't delete entry "${entry}"`), null);
+		});
+	};
 
 	module.exports = (entry, onfinish, onaction) =>
 		_rm(resolvePath(entry), _getfunc(onfinish, THROWIF), _getfunc(onaction, DONOTHING));
