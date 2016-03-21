bd76450ac7397f9546077709cc6c0684938e1992
khai96_ <hvksmr1996@gmail.com>
2016-03-21 22:18:17 +0700

    complete write-file-sync

--- write-file-sync.js
+++ write-file-sync.js
@@ -2,6 +2,51 @@
 ((module) => {
 	'use strict';
 
-	
+	var fs = require('fs');
+	var path = require('path');
+	var justTry = require('just-try');
+	var _mkdirSync = require('./mkdir-sync.js');
+	var _rmSync = require('./delete-sync.js');
+	var _getfunc = require('./utils/get-val.js').function;
+	var Info = require('./utils/info.js');
+	var Action = require('./utils/action.js');
+	var _donothing = require('./utils/do-nothing.js');
+	var flatArray = require('./utils/flat-array.js');
+	var _getdesc = require('./utils/write-file-desc.js');
+
+	var statSync = fs.statSync;
+	var writeFileSync = fs.writeFileSync;
+	var resolvePath = path.resolve;
+	var getParent = path.dirname;
+
+	var _writeFileSync = (filename, descriptor, onaction) => {
+		var callOnAction = (action) =>
+			justTry(() => onaction(action), (error) => console.error(error));
+		var createInfo = (...action) =>
+			new Info('mkfile', filename, action);
+		var data = descriptor.data;
+		var options = descriptor.options;
+		var mkdiract = _mkdirSync(getParent(filename), onaction).action;
+		return justTry(() => statSync(filename), () => write(), handleExist);
+		function handleExist(statinfo) {
+			if (statinfo.isDirectory()) {
+				let rmact = _rmSync(filename, onaction).action;
+				return write(...flatArray(rmact));
+			}
+			if (statinfo.isFile()) {
+				return write();
+			}
+			throw new Error(`Can't write "${filename}" as a file`);
+		}
+		function write(...nextact) {
+			writeFileSync(filename, data, options);
+			var action = new Action('create', filename, 'file');
+			justTry(onaction, [action]);
+			return createInfo(...flatArray(mkdiract), ...nextact, action);
+		}
+	};
+
+	module.exports = (filename, descriptor, onaction) =>
+		_writeFileSync(resolvePath(filename), _getdesc(descriptor), _getfunc(onaction, _donothing));
 
 })(module);
