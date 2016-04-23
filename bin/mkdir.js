1c2773cffebb2463bfe18e80732755df99494301
khai96_ <hvksmr1996@gmail.com>
2016-04-23 08:31:46 +0700

    complete bin/mkdir

---
+++ bin/mkdir.js
@@ -0,0 +1,33 @@
+#! usr/bin/env node --es-staging
+
+(() => {
+    'use strict';
+
+    var resolvePath = require('path').resolve;
+    var process = require('process');
+    var stract = require('fs-force-action-as-string');
+    var mkdir = require('../mkdir.js');
+
+    const EXIT_SUCCESS = 0;
+    const EXIT_FAILURE = 1;
+
+    process.argv.slice(2)
+        .map((dirname) => resolvePath(dirname))
+        .forEach((dirname) => mkdir(dirname, onfinish, onaction))
+    ;
+
+    function onfinish(error, info) {
+        if (error) {
+            console.error(error);
+            process.exit(EXIT_FAILURE);
+        } else {
+            console.log(`Created directory ${info.path}`);
+            process.exit(EXIT_SUCCESS);
+        }
+    }
+
+    function onaction(action) {
+        console.log(stract(action));
+    }
+
+})();
