df8a291cdfc222eec12306cb5737d51e19fcec65
khai96_ <hvksmr1996@gmail.com>
2016-05-14 11:09:50 +0700

    now I know where's the fault

--- bin/onfinish.js
+++ bin/onfinish.js
@@ -4,16 +4,11 @@
 
     var process = require('process');
 
-    const EXIT_SUCCESS = 0;
-    const EXIT_FAILURE = 1;
-
     module.exports = (action) => (error, info) => {
         if (error) {
             console.error(error);
-            process.exit(EXIT_FAILURE);
         } else {
             console.log(`${action} ${info.path} successfully`);
-            process.exit(EXIT_SUCCESS);
         }
     };
 
